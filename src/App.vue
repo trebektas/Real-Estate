35264a7770e557bbf630353b6cb382968a737f19
trebektas <trensarbektas@gmail.com>
2023-08-22 23:14:07 +0200

    add houses overview with data

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,35 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import HeaderItem from './components/headerItem.vue'
+import HeaderItem from './components/HeaderItem.vue'
 import HeaderItemMobile from './components/HeaderItemMobile.vue'
 </script>
 
 <template>
   <HeaderItem v-if="!isMobile()" />
   <HeaderItemMobile v-else />
-  <RouterView />
+  <main><RouterView /></main>
 </template>
 
 <script>
 export default {
   methods: {
     isMobile() {
-      if (window.innerWidth <= 900) return true
+      if (window.innerWidth <= 375) return true
       else return false
     }
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+main {
+  width: 100%;
+  padding-top: 100px;
+}
+
+@media only screen and (max-width: 375px) {
+  main {
+    padding-top: 10px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+import HousesOverview from '../components/HousesOverview.vue'
+</script>
 
 <template>
-  <main>
+  <div class="wrapper">
     <h1>This is a home page</h1>
-  </main>
+    <HousesOverview />
+  </div>
 </template>
+
+<style></style>
