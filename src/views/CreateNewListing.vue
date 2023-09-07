bb8ce7ef883e913dc7ffe8e3ef82e78e32f7a324
trebektas <trensarbektas@gmail.com>
2023-09-07 15:14:03 +0200

    adding dotenv to gitignore

--- src/views/CreateNewListing.vue
+++ src/views/CreateNewListing.vue
@@ -7,6 +7,7 @@ import { Field, Form, ErrorMessage } from 'vee-validate'
 import * as Yup from 'yup'
 import { useRouter } from 'vue-router'
 
+const MAX_FILE_SIZE = 7340032
 const router = useRouter()
 
 const validationSchema = Yup.object({
@@ -19,7 +20,13 @@ const validationSchema = Yup.object({
   numberAddition: Yup.string().trim(),
   zip: Yup.string().required('Required field missing.').trim(),
   city: Yup.string().required('Required field missing.').trim(),
-  image: Yup.mixed().required('Required field missing.'),
+  image: Yup.mixed()
+    .required('Required field missing.')
+    .test(
+      'is-valid-size',
+      'Max allowed size is 7MB',
+      (value) => value && value.size <= MAX_FILE_SIZE
+    ),
   price: Yup.number()
     .required('Required field missing.')
     .typeError('Required field must be a number.')
@@ -46,7 +53,9 @@ const validationSchema = Yup.object({
     .typeError('Required field must be a number.')
     .integer('Must be a whole number (no decimals)')
     .positive('Must be a positive number')
-    .test('len', 'Must be exactly 4 characters', (val) => val.toString().length === 4),
+    .test('len', 'Must be exactly 4 characters', (val) => val.toString().length === 4)
+    .min(1901, 'Must be greater than or equal to 1901')
+    .max(2023, 'Must be less than or equal to 2023'),
   description: Yup.string().required('Required field missing.')
 })
 
